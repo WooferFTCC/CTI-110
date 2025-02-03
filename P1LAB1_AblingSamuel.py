89d4631fd994193966091bc90a07c0ef0f49d4cb
WooferFTCC <ablings1235@student.faytechcc.edu>
2025-02-03 10:09:09 -0500

    Added P1LAB1 py script

---
+++ P1LAB1_AblingSamuel.py
@@ -0,0 +1,12 @@
+# Samuel Abling
+# 02/03/2025
+# P1LAB1
+# Program asks user to input their name, then returns a welcome message.
+
+# Ask input for First then Last names
+namef = input('Enter your first name: ')
+namel = input('Enter your last name: ')
+
+# Print welcome message with first and last names
+
+print('Hello', namef, namel + '! Welcome to CTI-110!')
