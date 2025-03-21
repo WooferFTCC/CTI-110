913bb77abffdd9742fa84939c4ed92255c52b373
WooferFTCC <ablings1235@student.faytechcc.edu>
2025-03-21 15:18:04 -0400

    Added P4LAB2

---
+++ P4LAB2_AblingSamuel.py
@@ -0,0 +1,36 @@
+# Samuel Abling
+# P4LAB2
+# 03/21/2025
+# Requests a positive integer then prints that int's multiplication table from 1 - 12.
+
+
+# Program runs in loop until cont(inue) == no
+while True:
+    
+    # Loop checking userIn until the user inputs a positive integer as a string.
+    userIn = ''
+    while not userIn.isnumeric():
+        userIn = input('\nEnter a nonnegative integer: ')
+
+        # Check if input string is negative.
+        if userIn[0] == '-':
+            print('\nThis program does not accept negative integers.')
+        # Check if input is an integer.
+        elif userIn.isnumeric() == False:
+            print('\nPlease type an integer.')
+
+    # Convert input to an int
+    num = int(userIn)
+
+
+    # Print multiplication table
+    print('\n')
+    for x in range(12):
+        print(f'{num} * {x + 1} = {num * (x + 1)}')
+
+    # Ask to run program again with new input.
+    cont = input('\nWould you like to run the program again? (yes / no) ')
+    if cont == 'no':
+        print('\n')
+        break
+
